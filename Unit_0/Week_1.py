f79ccb783dc567164637570b8a67d0406a19c85c
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2024-04-08 23:50:54 -0300

    Create Week_1.py

---
+++ Unit_0/Week_1.py
@@ -0,0 +1,29 @@
+"""
+CODING DORS
+UNIT 0 - FUNCTIONS AND VARIABLES - https://cs50.harvard.edu/python/2022/notes/0/
+WEEK 1 - Strings - https://www.codingdors.com/week/1
+"""
+
+# https://www.codingdors.com/problem/121
+"""
+first_character
+Show Solution
+Write a function that returns the first character of a given string.
+
+first_character('hello') -> 'h'
+
+first_character('world') -> 'w'
+
+first_character('CodingDors') -> 'C'
+"""
+# My Code
+  
+# Solution
+
+# IA's Solutions
+
+
+
+
+
+# https://www.codingdors.com/problem/
