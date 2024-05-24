529ca3811796d0211e1c01547aa52dec92c1adda
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2024-05-24 01:20:58 -0300

    Create Week_3.py

---
+++ Unit_1/Week_3.py
@@ -0,0 +1,74 @@
+"""
+CODING DORS
+UNIT 1 - CONDITIONALS - https://cs50.harvard.edu/python/2022/notes/1/
+WEEK 3 - Conditionals 2 - https://www.codingdors.com/week/3
+"""
+
+
+
+
+
+# https://www.codingdors.com/problem/
+"""
+
+"""
+# My Code
+  
+# Solution
+
+
+
+
+
+# https://www.codingdors.com/problem/
+"""
+
+"""
+# My Code
+  
+# Solution
+
+
+
+
+
+# https://www.codingdors.com/problem/
+"""
+
+"""
+# My Code
+  
+# Solution
+
+
+
+
+
+# https://www.codingdors.com/problem/
+"""
+
+"""
+# My Code
+  
+# Solution
+
+
+
+
+
+
+# https://www.codingdors.com/problem/
+"""
+
+"""
+# My Code
+  
+# Solution
+
+
+
+
+
+
+
+
