b0f56bf16a24424bc14e8b063e01d8c513843f73
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2024-06-03 02:13:16 -0300

    Create Week_6.py

---
+++ Unit_2/Week_6.py
@@ -0,0 +1,331 @@
+"""
+CODING DORS
+UNIT 2 - LOOPS - https://cs50.harvard.edu/python/2022/notes/2/
+WEEK 6 - Strings 2 - https://www.codingdors.com/week/6
+"""
+
+
+
+
+
+# https://www.codingdors.com/problem/
+"""
+
+"""
+# My Code
+
+
+# Solution
+
+
+# IA's Solutions
+
+
+
+
+
+# https://www.codingdors.com/problem/
+"""
+
+"""
+# My Code
+
+
+# Solution
+
+
+# IA's Solutions
+
+
+
+
+
+# https://www.codingdors.com/problem/
+"""
+
+"""
+# My Code
+
+
+# Solution
+
+
+# IA's Solutions
+
+
+
+
+
+# https://www.codingdors.com/problem/
+"""
+
+"""
+# My Code
+
+
+# Solution
+
+
+# IA's Solutions
+
+
+
+
+
+# https://www.codingdors.com/problem/
+"""
+
+"""
+# My Code
+
+
+# Solution
+
+
+# IA's Solutions
+
+
+
+
+
+# https://www.codingdors.com/problem/
+"""
+
+"""
+# My Code
+
+
+# Solution
+
+
+# IA's Solutions
+
+
+
+
+
+# https://www.codingdors.com/problem/
+"""
+
+"""
+# My Code
+
+
+# Solution
+
+
+# IA's Solutions
+
+
+
+
+
+# https://www.codingdors.com/problem/
+"""
+
+"""
+# My Code
+
+
+# Solution
+
+
+# IA's Solutions
+
+
+
+
+
+# https://www.codingdors.com/problem/
+"""
+
+"""
+# My Code
+
+
+# Solution
+
+
+# IA's Solutions
+
+
+
+
+
+# https://www.codingdors.com/problem/
+"""
+
+"""
+# My Code
+
+
+# Solution
+
+
+# IA's Solutions
+
+
+
+
+
+# https://www.codingdors.com/problem/
+"""
+
+"""
+# My Code
+
+
+# Solution
+
+
+# IA's Solutions
+
+
+
+
+
+# https://www.codingdors.com/problem/
+"""
+
+"""
+# My Code
+
+
+# Solution
+
+
+# IA's Solutions
+
+
+
+
+
+# https://www.codingdors.com/problem/
+"""
+
+"""
+# My Code
+
+
+# Solution
+
+
+# IA's Solutions
+
+
+
+
+
+# https://www.codingdors.com/problem/
+"""
+
+"""
+# My Code
+
+
+# Solution
+
+
+# IA's Solutions
+
+
+
+
+
+# https://www.codingdors.com/problem/
+"""
+
+"""
+# My Code
+
+
+# Solution
+
+
+# IA's Solutions
+
+
+
+
+
+# https://www.codingdors.com/problem/
+"""
+
+"""
+# My Code
+
+
+# Solution
+
+
+# IA's Solutions
+
+
+
+
+
+# https://www.codingdors.com/problem/
+"""
+
+"""
+# My Code
+
+
+# Solution
+
+
+# IA's Solutions
+
+
+
+
+
+# https://www.codingdors.com/problem/
+"""
+
+"""
+# My Code
+
+
+# Solution
+
+
+# IA's Solutions
+
+
+
+
+
+# https://www.codingdors.com/problem/
+"""
+
+"""
+# My Code
+
+
+# Solution
+
+
+# IA's Solutions
+
+
+
+
+
+# https://www.codingdors.com/problem/
+"""
+
+"""
+# My Code
+
+
+# Solution
+
+
+# IA's Solutions
+
+
+
+
+
+
