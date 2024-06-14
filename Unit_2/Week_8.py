cb1a4c03a064b1f0edf82a0db05fb08426601430
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2024-06-14 02:32:10 -0300

    Create Week_8.py

---
+++ Unit_2/Week_8.py
@@ -0,0 +1,356 @@
+"""
+CODING DORS
+UNIT 2 - LOOPS - https://cs50.harvard.edu/python/2022/notes/2/
+WEEK 8 - Dictionaries - https://www.codingdors.com/week/8
+"""
+
+
+
+
+
+# https://www.codingdors.com/problem/312
+"""
+create_dictionary
+Show Solution
+Given a string 'name' and an integer 'age', return a dictionary with the pair of key 'name' and value 'age' added. 
+Hint: Dictionaries are used to store data values in key:value pairs.
+create_dictionary(gi', 25) -> {'gi': 25}
+create_dictionary('leo', 41) -> {'leo': 41}
+create_dictionary('rod', 12) -> {'rod': 12}
+"""
+# My Code
+
+        
+# Solution
+
+
+# IA's Solutions
+
+
+
+
+
+
+# https://www.codingdors.com/problem/
+"""
+
+"""
+# My Code
+
+        
+# Solution
+
+
+# IA's Solutions
+
+
+
+
+
+
+# https://www.codingdors.com/problem/
+"""
+
+"""
+# My Code
+
+        
+# Solution
+
+
+# IA's Solutions
+
+
+
+
+
+
+# https://www.codingdors.com/problem/
+"""
+
+"""
+# My Code
+
+        
+# Solution
+
+
+# IA's Solutions
+
+
+
+
+
+
+# https://www.codingdors.com/problem/
+"""
+
+"""
+# My Code
+
+        
+# Solution
+
+
+# IA's Solutions
+
+
+
+
+
+
+# https://www.codingdors.com/problem/
+"""
+
+"""
+# My Code
+
+        
+# Solution
+
+
+# IA's Solutions
+
+
+
+
+
+
+# https://www.codingdors.com/problem/
+"""
+
+"""
+# My Code
+
+        
+# Solution
+
+
+# IA's Solutions
+
+
+
+
+
+
+# https://www.codingdors.com/problem/
+"""
+
+"""
+# My Code
+
+        
+# Solution
+
+
+# IA's Solutions
+
+
+
+
+
+
+# https://www.codingdors.com/problem/
+"""
+
+"""
+# My Code
+
+        
+# Solution
+
+
+# IA's Solutions
+
+
+
+
+
+
+# https://www.codingdors.com/problem/
+"""
+
+"""
+# My Code
+
+        
+# Solution
+
+
+# IA's Solutions
+
+
+
+
+
+
+# https://www.codingdors.com/problem/
+"""
+
+"""
+# My Code
+
+        
+# Solution
+
+
+# IA's Solutions
+
+
+
+
+
+
+# https://www.codingdors.com/problem/
+"""
+
+"""
+# My Code
+
+        
+# Solution
+
+
+# IA's Solutions
+
+
+
+
+
+
+# https://www.codingdors.com/problem/
+"""
+
+"""
+# My Code
+
+        
+# Solution
+
+
+# IA's Solutions
+
+
+
+
+
+
+# https://www.codingdors.com/problem/
+"""
+
+"""
+# My Code
+
+        
+# Solution
+
+
+# IA's Solutions
+
+
+
+
+
+
+# https://www.codingdors.com/problem/
+"""
+
+"""
+# My Code
+
+        
+# Solution
+
+
+# IA's Solutions
+
+
+
+# https://www.codingdors.com/problem/
+"""
+
+"""
+# My Code
+
+        
+# Solution
+
+
+# IA's Solutions
+
+
+
+
+
+
+# https://www.codingdors.com/problem/
+"""
+
+"""
+# My Code
+
+        
+# Solution
+
+
+# IA's Solutions
+
+
+
+
+
+
+# https://www.codingdors.com/problem/
+"""
+
+"""
+# My Code
+
+        
+# Solution
+
+
+# IA's Solutions
+
+
+
+
+
+
+# https://www.codingdors.com/problem/
+"""
+
+"""
+# My Code
+
+        
+# Solution
+
+
+# IA's Solutions
+
+
+
+
+
+
+
+
+
+# https://www.codingdors.com/problem/
+"""
+
+"""
+# My Code
+
+        
+# Solution
+
+
+# IA's Solutions
+
+
+
+
+
+
